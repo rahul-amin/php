1fe0ae70b38ed7d2bc3467646e5cbaa597afddf0
rahul amin roktim <rahulaminroktim@gmail.com>
2021-10-30 02:51:48 +0600

    Update response.php

--- response.php
+++ response.php
@@ -6,3 +6,28 @@ $response_data = [
     'server' => [], // server alert
     'data' => [], // data for json
 ];
+$common_response = new common_response();
+
+class common_response{
+    public $status = 200;
+    public $alerts = [];
+    public $server = [];
+    public $data = [];
+
+
+    function print_json($exit = 1)
+    {
+        $response_data = [
+            'status' => $this->status,
+            'alerts' => $this->alerts, // user alert
+            'server' => $this->server, // server alert
+            'data' => $this->data, // data for json
+        ];
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode($response_data);
+        if($exit)
+        {
+            exit;
+        }
+    }
+}
