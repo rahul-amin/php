19e7c0e3ce68d26bdb938158f1e313683a6361e6
rahul amin roktim <rahulaminroktim@gmail.com>
2021-11-05 05:25:28 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -65,3 +65,26 @@ function func_get_content($myurl, $method = 'get', $posts = [], $headers = [],$e
 	//  return mb_convert_encoding($result, 'UTF-8','auto');
 }
 
+function humanTiming ($time)
+{
+
+    $time = time() - $time; // to get the time since that moment
+    $time = ($time<1)? 1 : $time;
+    $tokens = array (
+        31536000 => 'year',
+        2592000 => 'month',
+        604800 => 'week',
+        86400 => 'day',
+        3600 => 'hour',
+        60 => 'minute',
+        1 => 'second'
+    );
+
+    foreach ($tokens as $unit => $text) {
+        if ($time < $unit) continue;
+        $numberOfUnits = floor($time / $unit);
+        return $numberOfUnits.' '.$text.(($numberOfUnits>1)?'s':'');
+    }
+
+}
+
